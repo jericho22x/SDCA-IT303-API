640d9955de69bb4a4041e45e90f32ff1d106425d
Jericho Tibayan <tyunako@gmail.com>
2025-08-30 15:34:42 +0800

    Initial Web Server Code

---
+++ index.js
@@ -0,0 +1,11 @@
+import { createServer } from 'node:http';
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/bold');
+  res.end('Hello BSIT3B');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
